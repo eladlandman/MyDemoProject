5c510b242c5526003fa77cadefbf6ee1b1c4be74
eladlandman <eland.landman@intel.com>
2020-09-30 21:28:21 +0300

    First Commit

---
+++ ClassLibrary1/Ieven.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+namespace MyDemoProject
+{
+    public  interface Ieven
+    {
+         bool IsEven();
+    }
+
+}
---
+++ ClassLibrary1/MyNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace MyDemoProject
+{
+    class MyNumber:Ieven
+    {
+
+        private int _number;
+
+        MyNumber(int number)
+        {
+            _number = number;
+        }
+
+        public bool IsEven()
+        {
+            return _number % 2 == 0;
+        }
+    }
+}
